8a1564e18577d3e54805b02052dccc369e1cf261
ttonchev <ttonchev@axway.com>
2014-05-23 11:48:06 +0300

    initial commit - add parse 1.5.0

---
+++ src/com/parse/starter/ParseApplication.java
@@ -0,0 +1,33 @@
+package com.parse.starter;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// Add your initialization code here
+		Parse.initialize(this, "tlnGs4HObIIQR1fk9oKx6ISdS45J6fGbEu0Wvqww", "r0wa5xXwSz1sZOr2gobBT5GQ82Sk7ZM4AtyliIQl");
+		
+
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+	    
+		// If you would like all objects to be private by default, remove this line.
+		defaultACL.setPublicReadAccess(true);
+		
+		ParseACL.setDefaultACL(defaultACL, true);
+		
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("foo", "bar");
+		testObject.saveInBackground();
+	}
+
+}
